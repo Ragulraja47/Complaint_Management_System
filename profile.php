6964b8a382a1e6cedd89a7a236e2ecca6cda6296
Saran0605 <saransoundharajan05@gmail.com>
2024-09-16 12:31:41 +0530

    Profile.php updated

--- profile.php
+++ profile.php
@@ -144,16 +144,16 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="p-t-30">
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="index.html" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
+                                href="index.php" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
                                     class="hide-menu">Dashboard</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="form-basic.html" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span
+                                href="profile.php" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span
                                     class="hide-menu">Profile</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="widgets.html" aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span
+                                href="worker_taskhistory.php" aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span
                                     class="hide-menu">Task History</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="tables.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span
+                                href="worker_helpline.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span
                                     class="hide-menu">Helpline</span></a></li>
 
                         </li>
