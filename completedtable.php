89411c907e4f7a7a9bf035ba40a6235a6e4fb34f
Saran0605 <saransoundharajan05@gmail.com>
2024-11-08 10:01:55 +0530

    completed table edited in main model added

--- completedtable.php
+++ completedtable.php
@@ -493,6 +493,26 @@ $row_count4 = mysqli_num_rows($result4);
                                     </div>
                                     <!--pending work modal end -->
 
+                                    <div class="modal fade" id="lettermodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">letter pad</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
                                     <!-- Pending table Start-->
                                     <div class="row">
                                         <div class="col-md-12">
