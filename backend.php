2a0a6fba90537f262f2838549e2b45bf7270fdff
Saran0605 <saransoundharajan05@gmail.com>
2024-12-28 15:24:18 +0530

    worker_id added in complaints detail

--- backend.php
+++ backend.php
@@ -121,23 +121,13 @@ if (isset($_POST['update'])) {
     $wname = $_POST['w_name'];
     $name = current(array_filter([$oname, $wname]));
 
-    
-
- 
-        
-   
-    
-    
-    
     $insertQuery = "UPDATE manager SET worker_id='$name' WHERE task_id='$taskId'";
     if (mysqli_query($conn, $insertQuery)) {
-        $updateQuery = "UPDATE complaints_detail SET status='7' WHERE id='$problem_id'";
-        if (mysqli_query($conn, $updateQuery)) {
           
         
             // Update status and task_completion in the complaints_detail table
             $updateComplaintSql = "UPDATE complaints_detail 
-                                   SET status = 11, task_completion = ?,reason = ?,date_of_completion = NOW()
+                                   SET status = 11,worker_id='$name', task_completion = ?,reason = ?,date_of_completion = NOW()
                                    WHERE id = (SELECT problem_id FROM manager WHERE task_id = ?)";
             if ($stmt = $conn->prepare($updateComplaintSql)) {
                 $stmt->bind_param("ssi", $completionStatus,$reason,$taskId);
@@ -183,7 +173,7 @@ if (isset($_POST['update'])) {
                 echo "No file uploaded or file upload error.";
             }   
 
-        }
+        
     }
    
 
